2eeee7cf9715f574a1c24c4c17979f513bb0da28
Angela Restrepo <Angelita_r7@hotmail.com>
2017-10-09 00:07:51 -0500

    Procesador_2

--- fuseRelaunch.cmd
+++ fuseRelaunch.cmd
@@ -1,1 +1,1 @@
--intstyle "ise" -incremental -o "C:/Users/GEORGE/Pictures/Procesador_2/Procesador_2/Seu_TestBench_isim_beh.exe" -prj "C:/Users/GEORGE/Pictures/Procesador_2/Procesador_2/Seu_TestBench_beh.prj" "work.Seu_TestBench" 
+-intstyle "ise" -incremental -o "C:/Users/GEORGE/Pictures/Procesador_2/Procesador_2/NextPC_TestBench_isim_beh.exe" -prj "C:/Users/GEORGE/Pictures/Procesador_2/Procesador_2/NextPC_TestBench_beh.prj" "work.NextPC_TestBench" 
